13f3f87dd2f74874b22ec3badbae824145e6e991
Mjw1994 <king.mjw.999@gmail.com>
2021-06-13 20:45:33 +0300

    Starting on project

---
+++ src/App.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.Scanner;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class App {
+
+    public static void menu(){
+
+            System.out.println("Welcom to our company please choose your option \n 1: Add \n Delect");
+    }
+    public static void main(String[] args) {
+    try {
+
+        Scanner input = new Scanner(System.in); 
+        menu();
+        
+        String NameFile = input.nextLine();
+        //System.out.println("The Director of file is "+NameFile);
+        File f = new File (NameFile); 
+        for (int i=0; i<f.listFiles().length; i++){
+            System.out.println(f.listFiles()[i].getName());
+        }
+        
+            
+ 
+        
+    } catch (Exception ex) 
+         {
+         System.out.println(ex.getMessage());    
+         }
+    }
+    
+
+}
---
+++ src/run.java
@@ -0,0 +1,7 @@
+public class run{
+    public static void main(String[] args) {
+        App d1 = new App(05, 05, 2021);
+        System.out.println(d1);
+    }
+    
+}
